7aa908eb4c287810fd336b5034a28a9c633c6ad8
manro <roman.denisov@gmail.com>
2018-12-06 21:35:14 +0200

    fix

--- server/boot/create-init-data.js
+++ server/boot/create-init-data.js
@@ -20,6 +20,7 @@ module.exports = function(app) {
   });
   */
 
+  /*
   app.models.Customer.create([{
     email: 'manro@callan.com',
     firstName: 'Manro',
@@ -34,6 +35,7 @@ module.exports = function(app) {
     if (err) throw err;
 
   });
+  */
 
   /*
   app.models.Role.create([{
